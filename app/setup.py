ddc7339e6bf9c3969ae5e109fdcddf2e816a7dfd
Ben Cook <b@bencook.ca>
2014-11-09 21:14:05 -0800

    Update setup.py

--- app/setup.py
+++ app/setup.py
@@ -3,9 +3,8 @@ from setuptools import setup
 setup(
     name='Sight',
     version='1.0',
-    long_description=__doc__,
+    author='Ben Cook'
+    description="iTunes analytics",
     packages=['sight'],
-    include_package_data=True,
-    zip_safe=False,
     install_requires=['Flask']
 )
